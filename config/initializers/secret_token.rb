974bd40a5e6d6157f6fbe3b6818092112d5b414a
Rob Freiburger <rob@robfreiburger.com>
2013-07-27 13:08:38 -0700

    Initial Rails 4 on Heroku commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CerealHack3::Application.config.secret_key_base = 'a1e025819b2ca68123d81623600567fa71c474db6b0e419b9066b527c85960ca79f7a205a4d1a5ee900db379180c4aff0b7a2802088359dcf615eaf6727428b0'
