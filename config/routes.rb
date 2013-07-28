daaafde0b53872a69a6f474019338f8ac1ba16b0
Rob Freiburger <rob@robfreiburger.com>
2013-07-28 02:39:41 -0700

    Removes StaticPages controller. Homepage no longer static.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 CerealHack3::Application.routes.draw do
-  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
